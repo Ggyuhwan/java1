dd2d98dd3408380abf26880eb0f448a2c6faf713
dogcatquokka <dlwodn9910@naver.com>
2023-07-14 18:26:16 +0900

    어제오늘 자바 한거

---
+++ javaStudy/src/ch01_start/Hello.java
@@ -0,0 +1,17 @@
+package ch01_start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		//자동완성 단축키 ctrl + space
+		//리네임 F2
+		//실행 crtl + F11
+		//라인 복사 ctrl + alt + 방향키
+		System.out.println("Hello world");
+		
+		
+	}
+
+}

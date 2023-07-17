2bbef6a442449a0f4fdb4b0a871a1db3e9d616e6
dogcatquokka <dlwodn9910@naver.com>
2023-07-17 10:02:35 +0900

    자바 연산자 23.7.17

---
+++ javaStudy/src/practice/submit01/hahaha.java
@@ -0,0 +1,26 @@
+package practice.submit01;
+
+import java.util.Scanner;
+
+public class hahaha {
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 10;
+		int z;
+		x++;
+		++x;
+		System.out.println("x="+ x);
+		
+		y--;
+		--y;
+		System.out.println("y="+ y);
+		
+		z= x++;
+		System.out.println("z="+z);
+		System.out.println("x="+x);
+		
+		}
+	}
+
+

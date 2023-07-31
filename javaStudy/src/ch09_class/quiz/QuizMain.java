5e6e7647256dc83bcbafbbcc48076fec83ae1e79
dogcatquokka <dlwodn9910@naver.com>
2023-07-31 17:17:24 +0900

    자바용어, 싱글톤 패턴, 영화퀴즈 23.07.31

---
+++ javaStudy/src/ch09_class/quiz/QuizMain.java
@@ -0,0 +1,57 @@
+package ch09_class.quiz;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class QuizMain {
+	public static void main(String[] args) {
+		
+		// 영화 맞추기
+		// 명대사 출력
+		// 못맞추면 배우 출력
+		// 못맞추면 초성 출력
+		// 못맞추면 다음 영화로 넘어감
+		// movieDB class는 싱글톤 패턴으로 디자인되어
+		// 외부에서 new를 할 수 없고 내부에서 생성되어진 하나의 인스턴스만을 사용
+		MovieDB movieDB = MovieDB.getInstance();
+		Scanner scanner = new Scanner(System.in);
+		// moveList 메소드는 가져온 인스턴스로만 호출가능
+		ArrayList<Movie> movieList = movieDB.getMoveList();
+		
+		
+		Collections.shuffle(movieList);
+		
+		int score = 0;
+		for(int i=0; i <movieList.size(); i++) {
+			System.out.println(movieList.get(i).getQuotes());
+			System.out.println(">>> ");
+			String answer = scanner.nextLine();
+			if(answer.equals(movieList.get(i).getTitle())) {
+				System.out.println("정답 입니다.~!");
+				score +=3;
+				continue; // 다음 문제로 넘어감
+			}
+			System.out.println(movieList.get(i).getActors());
+			System.out.println(">>> ");
+			answer = scanner.nextLine();
+			if(answer.equals(movieList.get(i).getTitle())) {
+				System.out.println("정답 입니다.~!");
+				score+=1;
+				continue; // 다음 문제로 넘어감
+			}
+			System.out.println(movieList.get(i).getWord());
+			System.out.println(">>> ");
+			answer = scanner.nextLine();
+			if(answer.equals(movieList.get(i).getTitle())) {
+				System.out.println("정답 입니다.~!");
+				score+=1;
+				continue; // 다음 문제로 넘어감
+			}
+			System.out.println("떙 !! 틀렸습니다. 다음 문제로 넘어갑니다.");
+		}
+		System.out.println("점수는 :"+score);
+	
+}
+
+}
---
+++ javaStudy/src/연습용/BankAppplication.java
@@ -0,0 +1,134 @@
+package 연습용;
+
+import java.text.BreakIterator;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class BankAppplication {
+	private static Account []accountsArray =  new Account[100];  
+	private static Scanner scanner =  new Scanner(System.in);  
+
+	
+public static void main(String[] args) {
+	boolean run = true;
+	while (run) {
+		System.out.println("---------------------------------");
+		System.out.println("1.계좌생성|2.계좌목록|3.예금|4.출금|5.종료");
+		System.out.println("---------------------------------");
+		System.out.println("선택> ");
+		
+		int selectNo = scanner.nextInt();
+		if (selectNo == 1) {
+			creatAcoount();
+		}else if(selectNo ==2) {
+			accountList();
+	}else if(selectNo ==3) {
+			deposit();
+	}else if(selectNo ==4) {
+			withdrow();
+	}else if(selectNo ==5) {
+			run = false;
+	}
+		
+}
+	System.out.println("프로그램 종료");
+}
+ 	//계좌생성하기
+	private static void creatAcoount() {
+		System.out.println("---------");
+		System.out.println("계좌생성");
+		System.out.println("---------");
+		System.out.print("계좌번호: ");
+		String ano = scanner.next();
+		System.out.print("계좌주: ");
+		String owner = scanner.next();
+		System.out.println("초기입금금액: ");
+		int balance = scanner.nextInt();
+		Account newAccount = new Account(ano, owner, balance);
+		for (int i = 0 ; i < accountsArray.length; i++) {
+			if(accountsArray[i] == null) {
+				accountsArray[i] = newAccount;
+				System.out.println("결과: 계좌가 생성되었습니다");
+				break;
+			}
+		}
+		
+	}
+	// 계좌 목록 보기
+		private static void accountList() {
+			System.out.println("---------");
+			System.out.println("계좌목록");
+			System.out.println("---------");
+			for(int i=0; i < accountsArray.length; i++) {
+				Account account = accountsArray[i];
+				if(account != null) {
+					System.out.print(account.getAno());
+					System.out.print("   ");
+					System.out.print(account.getOwner());
+					System.out.print("   ");
+					System.out.print(account.getBalance());
+					System.out.println();
+				}
+			}
+			
+		}
+		// 예금하기
+		private static void deposit() {
+			System.out.println("---------");
+			System.out.println("예금");
+			System.out.println("---------");
+			System.out.print("계좌번호: ");
+			String ano = scanner.next();
+			System.out.print("예금액: ");
+			// 계좌번호로 Account 찾기
+			int money = scanner.nextInt();
+			Account account = findAccount(ano);
+			if(account == null) {
+				System.out.println("결과 : 계좌가 없습니다.");
+				return;
+			}
+			System.out.println("예금전 잔액: "+account.getBalance());
+			account.setBalance(account.getBalance()+money);
+			System.out.println("결과 : 예금이 성공되었습니다.");
+			//예금 후 잔액 출력
+			
+			System.out.println("예금후 잔액: "+account.getBalance());
+			
+			
+		}
+		//출금하기
+		private static void withdrow() {
+			System.out.println("---------");
+			System.out.println("출금");
+			System.out.println("---------");
+			System.out.print("계좌번호: ");
+			String ano = scanner.next();
+			System.out.print("출금액: ");
+			int money =scanner.nextInt();
+			Account account  = findAccount(ano);
+			if(account == null) {
+				System.out.println("결과 : 계좌가 없습니다.");
+				return;
+			}
+			account.setBalance(account.getBalance() - money);
+			System.out.println("결과 : 출금이 성공되었습니다.");
+			//예금 출금 후 잔액 출력
+			System.out.println("출금후 잔액: "+account.getBalance());
+		}
+		//계좌 찾기
+		private static Account findAccount(String ano) {
+			Account account = null;
+			for(int i = 0; i < accountsArray.length; i++) {
+				if(accountsArray[i] !=null) {
+					String dbAno = accountsArray[i].getAno();
+					if(dbAno.equals(ano)) {
+						account = accountsArray[i];
+						break;
+					}
+				}
+			}
+			return account;
+			
+		}
+}
+

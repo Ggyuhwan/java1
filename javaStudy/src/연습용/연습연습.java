b9fb78314935e8598e53d52d832d4e762edacaec
dogcatquokka <dlwodn9910@naver.com>
2023-08-09 21:17:34 +0900

    DB key 코딩문제풀이 23.08.09

--- javaStudy/src/연습용/연습연습.java
+++ javaStudy/src/연습용/연습연습.java
@@ -11,18 +11,16 @@ import org.json.simple.parser.ParseException;
 
 public class 연습연습 {
 	public static void main(String[] args) throws ParseException {
-	HashMap<String, Integer> coinMap = new HashMap<String, Integer>();
-	HashMap<String, String> stuMap = new HashMap<String, String>();
-	stuMap.put("첫째", "길동");
-	stuMap.put("둘째", "몽길");
-	stuMap.put("셋째", "홍길동");
-	
-	System.out.println(stuMap);
-	coinMap.put("비트코인", 36000000);
-	coinMap.put("이더리움", 10000000);
-	System.out.println(coinMap.get("비트코인"));
-	System.out.println(coinMap.size());
 	
  	}
+	class Solution {
+		String str1 = "aaaaa";
+		String str2 = "bbbbb";
+	    public String solution(String str1, String str2) {
+	    	str1.charAt(0);
+	        String answer = "";
+	        return answer;
+	    }
+	}
 
 }
